b40044afd896deb8abc517b92d1fbb3771348372
robbieve <robbieeve751994@gmail.com>
2018-05-29 20:03:15 +0300

    Add dashboard route and component to App.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { IntlProvider, addLocaleData } from 'react-intl';
+
 import Auth from './components/auth';
 import LandingPage from './components/landingPage';
+import Dashboard from './components/dashboard';
 
 //translation stuff
 import romanian from 'react-intl/locale-data/ro';
@@ -23,6 +25,7 @@ const CVRouter = (props) => {
       <Switch>
         <Route exact path='/:lang(en|ro)' component={LandingPage} />
         <Route exact path='/:lang(en|ro)/auth' component={Auth} />
+        <Route path='/:lang(en|ro)/dashboard' component={Dashboard} />
       </Switch>
     </IntlProvider>
   );
@@ -31,10 +34,10 @@ const CVRouter = (props) => {
 class App extends Component {
   render() {
     return (
-      <BrowserRouter >
+      <BrowserRouter>
         <Switch>
           <Route path='/:lang(en|ro)' component={CVRouter} />
-          <Route path='/:segment(auth|job|brand)' component={(props) => <Redirect to={`/${navigator.language.substr(0, 2)}/${props.location.pathname.replace(/^\/+/, "")}`} />} />
+          <Route path='/:segment(auth|dashboard)' component={(props) => <Redirect to={`/${navigator.language.substr(0, 2)}/${props.location.pathname.replace(/^\/+/, "")}`} />} />
           <Redirect from='/' to={'/en'} />
         </Switch>
       </BrowserRouter>
