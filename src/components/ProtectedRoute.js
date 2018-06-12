59ae1ffaa7a62079e20052da06b8235c4e24ae3e
robbieve <robbieeve751994@gmail.com>
2018-06-12 12:10:28 +0300

    Added App.scss and updated protected route.

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {Redirect, Route} from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import { IS_AUTHENTICATED } from '../store/queries';
 import { Query } from 'react-apollo';
 
 const ProtectedRoute = (props) =>
   <Query query={IS_AUTHENTICATED}>
-    { ({ data: { isAuthenticated } }) => {
-        alert(isAuthenticated);
-        return isAuthenticated ? <Route {...props}/> : <Redirect to="/auth"/>
-      }
-    }
+    {({ data: { isAuthenticated } }) => isAuthenticated ? <Route {...props} /> : <Redirect to="/auth" />}
   </Query>
 
 export default ProtectedRoute;
