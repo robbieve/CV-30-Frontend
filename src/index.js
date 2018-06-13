e3420c45f1af77e6d5215dbb50355822bad52ffa
robbieve <robbieeve751994@gmail.com>
2018-06-13 13:47:04 +0300

    npm updates

--- src/index.js
+++ src/index.js
@@ -1,55 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import ApolloClient from "apollo-boost";
-import ApolloClient from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from "apollo-boost";
+// import ApolloClient from 'apollo-client';
+// import { HttpLink } from 'apollo-link-http';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from "react-apollo";
-import { withClientState } from 'apollo-link-state';
-import { ApolloLink } from 'apollo-link';
+// import { withClientState } from 'apollo-link-state';
+// import { ApolloLink } from 'apollo-link';
 import { defaults, resolvers } from './store';
 
-// const client = new ApolloClient({
-//     uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
-//     fetchOptions: {
-//         credentials: 'same-origin'
-//     },
-//     onError: ({ graphQLErrors, networkError }) => {
-//         if (graphQLErrors) {
-//             // sendToLoggingService(graphQLErrors);
-//         }
-//         if (networkError) {
-//             // logoutUser();
-//         }
-//     },
-//     clientState: {
-//         defaults,
-//         resolvers
-//     }
+const client = new ApolloClient({
+    uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
+    fetchOptions: {
+        credentials: 'same-origin'
+    },
+    clientState: {
+        defaults,
+        resolvers
+    }
+});
+
+// const cache = new InMemoryCache();
+
+// const stateLink = withClientState({
+//     resolvers,
+//     cache,
+//     defaults
 // });
 
-const cache = new InMemoryCache();
+// const httpLink = new HttpLink({
+//     uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
+//     credentials: 'include'
+// });
 
-const stateLink = withClientState({
-    resolvers,
-    cache,
-    defaults
-});
-
-const httpLink = new HttpLink({
-    uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
-    credentials: 'include'
-});
-
-const client = new ApolloClient({
-    // link: createHttpLink({
-    //     uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
-    //     credentials: 'include'
-    // }),
-    cache,
-    link: ApolloLink.from([stateLink, httpLink])
-})
+// const client = new ApolloClient({
+//     // link: createHttpLink({
+//     //     uri: process.env.NODE_ENV === 'production' ? 'https://api.cv30.co/graphql' : 'http://localhost/graphql',
+//     //     credentials: 'include'
+//     // }),
+//     cache,
+//     link: ApolloLink.from([stateLink, httpLink])
+// })
 
 ReactDOM.render(
     <ApolloProvider client={client}>
