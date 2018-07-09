3b4c0e92e0a97abc13a897eb0181f0252fb612d0
robbieve <robbieeve751994@gmail.com>
2018-07-09 18:05:25 +0300

    Add file upload to experience and projects editors.

--- src/pages/users/userProfile/components/experienceEdit.js
+++ src/pages/users/userProfile/components/experienceEdit.js
@@ -4,11 +4,217 @@ import { compose, pure, withState, withHandlers, lifecycle } from 'recompose';
 import { graphql } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
 import uuid from 'uuid/v4';
+import S3Uploader from 'react-s3-uploader';
 
 import { setExperience, setProject, currentUserQuery, googleMapsQuery } from '../../../../store/queries';
 
+const ExperienceEditHOC = compose(
+    withRouter,
+    graphql(setExperience, { name: 'setExperience' }),
+    graphql(setProject, { name: 'setProject' }),
+    graphql(googleMapsQuery, { name: 'googleMapsData' }),
+    withState('isAutocompleteInit', 'setAutocompleteInit', false),
+    withState('autocompleteHandle', '', () => React.createRef()),
+    withState('formData', 'setFormData', ({ job }) => {
+        if (!job) {
+            return {
+                id: uuid(),
+                video: {
+                    name: 'video',
+                    path: ''
+                }
+            };
+        }
+
+        const { id, company, position, location, startDate, endDate, isCurrent, i18n, videos } = job;
+        const { description } = i18n && i18n[0];
+
+        let data = {
+            id, company, position, location, startDate, endDate, isCurrent, description, video: !!videos.length ? { ...videos[0], name: 'video' } : { name: 'video', path: '' }
+        };
+        return data;
+    }),
+    withState('isVideoUrl', 'changeMediaType', true),
+    withState('isSaving', 'setIsSaving', false),
+    withState('uploadProgress', 'setUploadProgress', 0),
+    withState('uploadError', 'setUploadError', null),
+    withHandlers({
+        handleFormChange: props => event => {
+            if (typeof event.name != undefined && event.name == 'video') {
+                props.setFormData(state => ({ ...state, video: event }));
+                return;
+            }
+
+            const target = event.currentTarget;
+            const name = target.name;
+            const value = target.type === 'checkbox' ? target.checked : target.value;
+            if (!name) {
+                throw Error('Field must have a name attribute!');
+            }
+            props.setFormData(state => ({ ...state, [name]: value }));
+        },
+        switchMediaType: ({ isVideoUrl, changeMediaType }) => () => {
+            changeMediaType(!isVideoUrl);
+        },
+        submitForm: ({ formData, setExperience, setProject, type, match, closeEditor }) => async () => {
+            let { id, title, description, position, company, startDate, endDate, isCurrent, video, location } = formData;
+            const videos = [];
+            if (video.path && !!video.path.length) {
+                videos.push({
+                    id: video.id ? video.id : uuid(),
+                    source: id,
+                    path: video.path ? video.path : '',
+                    sourceType: type
+                });
+            }
+            switch (type) {
+                case 'experience':
+                    try {
+                        await setExperience({
+                            variables: {
+                                experience: {
+                                    id, location, title, description, position, company, startDate, endDate, isCurrent, videos
+                                },
+                                language: match.params.lang
+                            },
+                            refetchQueries: [{
+                                query: currentUserQuery,
+                                fetchPolicy: 'network-only',
+                                name: 'currentUser',
+                                variables: {
+                                    language: match.params.lang
+                                }
+                            }]
+                        });
+                    }
+                    catch (err) {
+                        console.log(err);
+                    }
+                    closeEditor();
+                    break;
+                case 'project':
+                    try {
+                        await setProject({
+                            variables: {
+                                project: {
+                                    id, location, title, description, position, company, startDate, endDate, isCurrent, videos
+                                },
+                                language: match.params.lang
+                            },
+                            refetchQueries: [{
+                                query: currentUserQuery,
+                                fetchPolicy: 'network-only',
+                                name: 'currentUser',
+                                variables: {
+                                    language: match.params.lang
+                                }
+                            }]
+                        });
+                    }
+                    catch (err) {
+                        console.log(err);
+                    }
+                    closeEditor();
+                    break;
+                default:
+                    return false;
+            }
+        },
+        initAutocomplete: ({ isAutocompleteInit, setAutocompleteInit, autocompleteHandle, setFormData }) => () => {
+            if (isAutocompleteInit) return;
+            const autocompleteInstance = new window.google.maps.places.Autocomplete(autocompleteHandle.current);
+            autocompleteInstance.addListener('place_changed', () => {
+                const place = autocompleteInstance.getPlace();
+                if (!place.geometry) {
+                    window.alert("No details available for input: '" + place.name + "'");
+                    return;
+                }
+                if (place) {
+                    setFormData(state => ({ ...state, 'location': place.formatted_address }));
+                }
+            });
+            setAutocompleteInit(true);
+        },
+        getSignedUrl: ({ formData, type }) => async (file, callback) => {
+            const params = {
+                fileName: file.name,
+                contentType: file.type,
+                id: formData.id,
+                type: type
+            };
+
+            try {
+                let response = await fetch('https://k73nyttsel.execute-api.eu-west-1.amazonaws.com/production/getSignedURL', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(params)
+                });
+                let responseJson = await response.json();
+                callback(responseJson);
+            } catch (error) {
+                console.error(error);
+                callback(error)
+            }
+        },
+        onUploadStart: ({ setIsSaving, formData, setFormData, type }) => (file, next) => {
+            let size = file.size;
+            if (size > 1024 * 1024) {
+                alert('File is too big!');
+            } else {
+                let newFormData = Object.assign({}, formData);
+
+                //add image to current formData
+                newFormData.images = [{
+                    id: uuid(),
+                    title: file.name,
+                    sourceType: type,
+                    source: formData.id, //article id
+                    path: `/articles/${formData.id}/${file.name}`
+                }];
+                setFormData(newFormData);
+                setIsSaving(true);
+                next(file);
+            }
+        },
+        onProgress: ({ setUploadProgress }) => (percent) => {
+            setUploadProgress(percent);
+        },
+        onError: ({ setIsSaving, setUploadError }) => error => {
+            console.log(error);
+            setUploadError(error);
+            setIsSaving(false);
+        },
+        onFinishUpload: (props) => async () => {
+            const { setIsSaving } = props;
+            console.log('success!');
+            setIsSaving(false);
+        }
+    }),
+    lifecycle({
+        componentDidMount() {
+            if (this.props.googleMapsData.googleMaps.isLoaded && window.google && !this.props.isAutocompleteInit) this.props.initAutocomplete();
+        },
+        componentDidUpdate() {
+            if (this.props.googleMapsData.googleMaps.isLoaded && window.google && !this.props.isAutocompleteInit) this.props.initAutocomplete();
+        }
+    }),
+    pure
+);
+
 const ExperienceEdit = (props) => {
-    const { formData, isVideoUrl, switchMediaType, handleFormChange, closeEditor, submitForm, type, autocompleteHandle } = props;
+    const {
+        formData,
+        isVideoUrl, switchMediaType,
+        handleFormChange,
+        closeEditor,
+        submitForm,
+        type,
+        autocompleteHandle,
+        getSignedUrl, onUploadStart, onProgress, onError, onFinishUpload, isSaving
+    } = props;
     const { position, company, location, startDate, endDate, isCurrent, description, video } = formData;
 
     return (
@@ -114,168 +320,36 @@ const ExperienceEdit = (props) => {
                         value={video && video.path || ''}
                     /> :
                     <label htmlFor="fileUpload">
-                        <input
-                            accept="image/*"
-                            className='hiddenFileInput'
+                        <S3Uploader
                             id="fileUpload"
                             name="fileUpload"
-                            multiple
-                            type="file"
-                            onChange={handleFormChange}
+                            className='hiddenInput'
+                            getSignedUrl={getSignedUrl}
+                            accept="image/*"
+                            preprocess={onUploadStart}
+                            onProgress={onProgress}
+                            onError={onError}
+                            onFinish={onFinishUpload}
+                            uploadRequestHeaders={{
+                                'x-amz-acl': 'public-read',
+                            }}
                         />
-                        <Button component="span" className='uploadBtn'>
+                        <Button component="span" className='uploadBtn' disabled={isSaving}>
                             Upload
                         </Button>
                     </label>
                 }
             </section>
             <section className='editControls'>
-                <IconButton className='cancelBtn' onClick={closeEditor}>
+                <IconButton className='cancelBtn' onClick={closeEditor} disabled={isSaving}>
                     <Icon>close</Icon>
                 </IconButton>
-                <IconButton className='submitBtn' onClick={submitForm}>
+                <IconButton className='submitBtn' onClick={submitForm} disabled={isSaving}>
                     <Icon>done</Icon>
                 </IconButton>
             </section>
         </form>
     )
 };
-
-const ExperienceEditHOC = compose(
-    withRouter,
-    graphql(setExperience, { name: 'setExperience' }),
-    graphql(setProject, { name: 'setProject' }),
-    graphql(googleMapsQuery, { name: 'googleMapsData' }),
-    withRouter,
-    withState('isAutocompleteInit', 'setAutocompleteInit', false),
-    withState('autocompleteHandle', '', () => React.createRef()),
-    withState('formData', 'setFormData', ({ job }) => {
-        if (!job)
-            return {
-                video: {
-                    name: 'video',
-                    path: ''
-                }
-            };
-
-        const { id, company, position, location, startDate, endDate, isCurrent, i18n, videos } = job;
-        const { description } = i18n && i18n[0];
-
-        let data = {
-            id, company, position, location, startDate, endDate, isCurrent, description, video: !!videos.length ? { ...videos[0], name: 'video' } : { name: 'video', path: '' }
-        };
-        return data;
-    }),
-    withState('isVideoUrl', 'changeMediaType', true),
-    withHandlers({
-        handleFormChange: props => event => {
-            if (typeof event.name != undefined && event.name == 'video') {
-                props.setFormData(state => ({ ...state, video: event }));
-                return;
-            }
-            
-            const target = event.currentTarget;
-            const name = target.name;
-            const value = target.type === 'checkbox' ? target.checked : target.value;
-            if (!name) {
-                throw Error('Field must have a name attribute!');
-            }
-            props.setFormData(state => ({ ...state, [name]: value }));
-        },
-        switchMediaType: ({ isVideoUrl, changeMediaType }) => () => {
-            changeMediaType(!isVideoUrl);
-        },
-        submitForm: ({ formData, setExperience, setProject, type, match, closeEditor }) => async () => {
-            let { id, title, description, position, company, startDate, endDate, isCurrent, video, location } = formData;
-            const videos = [];
-            if (video.path && !!video.path.length) {
-                videos.push({
-                    id: video.id ? video.id : uuid(),
-                    source: id,
-                    path: video.path ? video.path : '',
-                    sourceType: type
-                });
-                debugger;
-            }
-            switch (type) {
-                case 'experience':
-                    try {
-                        await setExperience({
-                            variables: {
-                                experience: {
-                                    id, location, title, description, position, company, startDate, endDate, isCurrent, videos
-                                },
-                                language: match.params.lang
-                            },
-                            refetchQueries: [{
-                                query: currentUserQuery,
-                                fetchPolicy: 'network-only',
-                                name: 'currentUser',
-                                variables: {
-                                    language: match.params.lang
-                                }
-                            }]
-                        });
-                    }
-                    catch (err) {
-                        console.log(err);
-                    }
-                    closeEditor();
-                    break;
-                case 'project':
-                    try {
-                        await setProject({
-                            variables: {
-                                project: {
-                                    id, location, title, description, position, company, startDate, endDate, isCurrent, videos
-                                },
-                                language: match.params.lang
-                            },
-                            refetchQueries: [{
-                                query: currentUserQuery,
-                                fetchPolicy: 'network-only',
-                                name: 'currentUser',
-                                variables: {
-                                    language: match.params.lang
-                                }
-                            }]
-                        });
-                    }
-                    catch (err) {
-                        console.log(err);
-                    }
-                    closeEditor();
-                    break;
-                default:
-                    return false;
-            }
-
-        },
-        initAutocomplete: ({ isAutocompleteInit, setAutocompleteInit, autocompleteHandle, setFormData }) => () => {
-            if (isAutocompleteInit) return;
-            const autocompleteInstance = new window.google.maps.places.Autocomplete(autocompleteHandle.current);
-            autocompleteInstance.addListener('place_changed', () => {
-                const place = autocompleteInstance.getPlace();
-                if (!place.geometry) {
-                    window.alert("No details available for input: '" + place.name + "'");
-                    return;
-                }
-                if (place) {
-                    setFormData(state => ({ ...state, 'location': place.formatted_address }));
-                }
-            });
-            setAutocompleteInit(true);
-        }
-    }),
-    lifecycle({
-        componentDidMount() {
-            if (this.props.googleMapsData.googleMaps.isLoaded && window.google && !this.props.isAutocompleteInit) this.props.initAutocomplete();
-        },
-        componentDidUpdate() {
-            if (this.props.googleMapsData.googleMaps.isLoaded && window.google && !this.props.isAutocompleteInit) this.props.initAutocomplete();
-        }
-    }),
-    pure
-);
 
 export default ExperienceEditHOC(ExperienceEdit);
