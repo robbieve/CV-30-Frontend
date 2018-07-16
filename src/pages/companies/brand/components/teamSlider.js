39346a1ed73af06b48d8decb2a009c02423665c6
robbieve <robbieeve751994@gmail.com>
2018-07-16 16:02:29 +0300

    functional team slide

--- src/pages/companies/brand/components/teamSlider.js
+++ src/pages/companies/brand/components/teamSlider.js
@@ -2,29 +2,42 @@ import React from 'react';
 // import Slider from "react-flex-carousel";
 import { Link } from 'react-router-dom';
 import { compose, withState, withHandlers, pure } from 'recompose';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const TeamSliderHOC = compose(
     withState(),
     withHandlers(),
-    // lifecycle(),
     pure
 )
 
 const TeamSlider = props => {
     const { teams, match: { params: { lang } } } = props;
-    return (
-        <div className='slidesContainer'>
-            {teams.map(team => {
+
+    return <CarouselProvider
+        dragEnabled={false}
+        visibleSlides={2} // dinamyc
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={teams.length}
+        className="teamSliderMain"
+        >
+        <Slider className="slidesContainer" classNameTrayWrap="slidesContainer">
+            {teams.map((team, index) => {
                 let url = `/${lang}/dashboard/team/${team.id}`;
                 return (
-                    <Link to={url} className='teamSliderItem' key={team.id}>
-                        <img src='https://www.projecttimes.com/media/k2/items/cache/6612da61425d98755836902a8bde1bce_XL.jpg' alt="ceva" className='teamImg' />
-                        <span className='teamText'>{team.name}</span>
-                    </Link>
-                )
+                    <Slide index={index} key={index}>
+                        <Link to={url} className='teamSliderItem' key={team.id}>
+                            <img src='https://www.projecttimes.com/media/k2/items/cache/6612da61425d98755836902a8bde1bce_XL.jpg' alt="ceva" className='teamImg' />
+                            <span className='teamText'>{team.name}</span>
+                        </Link>
+                    </Slide>
+                );
             })}
-        </div>
-    );
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>;
 }
 
 export default TeamSliderHOC(TeamSlider);
