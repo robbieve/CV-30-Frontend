ec19a2ab10b454470508da305b373cc38055853e
robbieve <robbieeve751994@gmail.com>
2018-08-30 18:28:21 +0300

    Update instructions to include file size and translated messages.

--- src/components/imageUploader/uploader.js
+++ src/components/imageUploader/uploader.js
@@ -3,6 +3,7 @@ import DropNCrop from '@synapsestudios/react-drop-n-crop';
 import '@synapsestudios/react-drop-n-crop/lib/react-drop-n-crop.min.css';
 import { compose, withState, withHandlers } from 'recompose';
 import { Button } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
 
 
 const uploadFile = async (image, signedUrl) => {
@@ -87,13 +88,51 @@ const ImageUploadHOC = compose(
 );
 
 const ImageUpload = ({ image, onChange, handleUploadFile, type }) => {
-    let maxFileSize = (type === 'company_logo') || (type === 'profile_avatar') ? 209715 : 3145728
+    let maxFileSize = (type === 'company_logo') || (type === 'profile_avatar') ? 204800 : 3145728;
+    let allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    let instructions = (
+        <div className="dropzone-instructions">
+            <FormattedMessage id="uploader.dragNDrop" defaultMessage="Drag-n-drop a file or click to add an image">
+                {(text) => (
+                    <div className="dropzone-instructions--main">
+                        {text}
+                    </div>
+                )}
+            </FormattedMessage>
+            <FormattedMessage id="uploader.acceptedFiles" defaultMessage="Accepted file types: ">
+                {(text) => (
+                    <div className="dropzone-instructions--sub">
+                        {text}
+                        {allowedFileTypes
+                            .map(mimeType => `.${mimeType.split('/')[1]}`)
+                            .join(', ')}
+                    </div>
+                )}
+            </FormattedMessage>
+            <FormattedMessage id="uploader.maxFileSize" defaultMessage="Max file size:">
+                {(text) => (
+                    <div className="dropzone-instructions--sub">
+                        {text} {(type === 'company_logo') || (type === 'profile_avatar') ? '200kB' : '3MB'}
+                    </div>
+                )}
+            </FormattedMessage>
+            <FormattedMessage id="uploader.imageSizes" defaultMessage="Recommended image size:">
+                {(text) => (
+                    <div className="dropzone-instructions--sub">
+                        {text} {(type === 'company_logo') || (type === 'profile_avatar') ? '200x200' : '1920x1080'}
+                    </div>
+                )}
+            </FormattedMessage>
+        </div>
+    );
     return (
         <div className='imageUpload'>
             <DropNCrop
                 onChange={onChange}
                 value={image}
                 maxFileSize={maxFileSize}
+                allowedFileTypes={allowedFileTypes}
+                instructions={instructions}
             />
             <Button onClick={handleUploadFile} className='uploadBtn' disabled={!image || !image.filename}>Upload</Button>
         </div>
