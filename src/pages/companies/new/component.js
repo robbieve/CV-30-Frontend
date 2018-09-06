5f2f4dffec853b9bba3fa6ecfdc3414d9e212241
robbieve <robbieeve751994@gmail.com>
2018-09-06 18:03:40 +0300

    Moved logic to container. Updated conditions for validations showing errors on inputs.

--- src/pages/companies/new/component.js
+++ src/pages/companies/new/component.js
@@ -1,65 +1,48 @@
 import React from 'react';
-import {
-    Grid, TextField, Button, CircularProgress
-    // Avatar, Icon, , IconButton, Field
-} from '@material-ui/core';
-// import S3Uploader from 'react-s3-uploader';
-import { graphql } from 'react-apollo';
-import { compose, pure } from 'recompose';
-import { withFormik } from 'formik';
-import { withRouter } from 'react-router-dom';
-
-import { setFeedbackMessage } from '../../../store/queries';
-import { companiesRefetch } from '../../../store/refetch';
-import schema from './validation';
+import { Grid, TextField, Button, CircularProgress } from '@material-ui/core';
 
 import IndustryInput from '../../../components/IndustryInput';
 import LocationInput from '../../../components/LocationInput';
 
-const NewCompany = props => {
-    const {
-        // getSignedUrl, onUploadStart, onProgress, onError, onFinishUpload, isUploading, uploadProgress,
-        cancel,
-        values, touched, errors, isSubmitting, handleChange, handleSubmit, isValid
-    } = props;
-
-    return (
-        <div className='newCompanyRoot'>
-            <Grid container className='header'>
-                <Grid item lg={6} sm={11}>
-                    <h1 className='headerTitle'>Create <b>company profile</b></h1>
-                    <p className='headerMessage'>Novum quando similique ei sed. Porro convenire scriptorem vim ei, an vim decore prodesset. Quem appetere delicatissimi vis ei, eos stet partem noster at, cu duo mazim utamur intellegebat. Mea no sint choro noluisse, pro duis dissentiet ea. Ea enim numquam nam.</p>
-                </Grid>
+const NewCompany = ({ cancel, values, touched, errors, isSubmitting, handleBlur, handleChange, handleSubmit, isValid }) => (
+    <div className='newCompanyRoot'>
+        <Grid container className='header'>
+            <Grid item lg={6} sm={11}>
+                <h1 className='headerTitle'>Create <b>company profile</b></h1>
+                <p className='headerMessage'>Novum quando similique ei sed. Porro convenire scriptorem vim ei, an vim decore prodesset. Quem appetere delicatissimi vis ei, eos stet partem noster at, cu duo mazim utamur intellegebat. Mea no sint choro noluisse, pro duis dissentiet ea. Ea enim numquam nam.</p>
             </Grid>
-            <Grid container className='mainBody newCompany'>
-                <Grid item lg={6} sm={11}>
-                    <section className='locationSection'>
-                        <LocationInput
-                            value={values.location}
-                            onChange={handleChange}
-                            error={touched.location && errors.location}
-                            helperText={errors.location}
-                        />
-                    </section>
-                    <section className='titleSection'>
-                        <TextField
-                            error={touched.name && errors.name}
-                            helperText={errors.name}
-                            name="name"
-                            label="Company name"
-                            placeholder="Enter company name..."
-                            className='textField'
-                            fullWidth
-                            onChange={handleChange}
-                            value={values.name}
-                            InputProps={{
-                                classes: {
-                                    input: 'titleInput',
-                                }
-                            }}
-                        />
-                    </section>
-                    {/* <section className='mediaSection'>
+        </Grid>
+        <Grid container className='mainBody newCompany'>
+            <Grid item lg={6} sm={11}>
+                <section className='locationSection'>
+                    <LocationInput
+                        value={values.location}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        error={!!(touched.location && errors.location)}
+                        helperText={touched.location && errors.location}
+                    />
+                </section>
+                <section className='titleSection'>
+                    <TextField
+                        name="name"
+                        label="Company name"
+                        placeholder="Enter company name..."
+                        className='textField'
+                        fullWidth
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.name}
+                        error={!!(touched.name && errors.name)}
+                        helperText={touched.name && errors.name}
+                        InputProps={{
+                            classes: {
+                                input: 'titleInput',
+                            }
+                        }}
+                    />
+                </section>
+                {/* <section className='mediaSection'>
                         <div className='avatar'>
                             <Avatar src='' alt='company pic' className='companyAvatarPreview' />
                             <label htmlFor="uploadProfileImg">
@@ -95,84 +78,40 @@ const NewCompany = props => {
                         </div>
                         <div className='colorPicker'></div>
                     </section> */}
-                    <section className='details'>
-                        <IndustryInput
-                            error={touched.industry && errors.industry}
-                            helperText={errors.industry}
-                            onChange={handleChange}
-                            value={values.industry}
-                        />
+                <section className='details'>
+                    <IndustryInput
+                        className='textField'
+                        fullWidth
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.industry}
+                        error={!!(touched.industry && errors.industry)}
+                        helperText={touched.industry && errors.industry}
+                    />
 
-                        <TextField
-                            error={touched.noOfEmployees && errors.noOfEmployees}
-                            helperText={errors.noOfEmployees}
-                            name="noOfEmployees"
-                            label="Number of employees"
-                            placeholder="Enter number of employees..."
-                            className='textField'
-                            fullWidth
-                            onChange={handleChange}
-                            value={values.noOfEmployees}
-                        />
-                    </section>
-                    <section className='actions'>
-                        {isSubmitting && <CircularProgress />}
-                        {!isSubmitting && <React.Fragment>
-                            <Button className='cancelBtn' onClick={cancel}>Cancel</Button>
-                            <Button className={isValid ? 'submitBtn' : ''} disabled={!isValid} onClick={handleSubmit}>Create company</Button>
-                        </React.Fragment>}
-                    </section>
-                </Grid>
+                    <TextField
+                        name="noOfEmployees"
+                        label="Number of employees"
+                        placeholder="Enter number of employees..."
+                        className='textField'
+                        fullWidth
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.noOfEmployees}
+                        error={!!(touched.noOfEmployees && errors.noOfEmployees)}
+                        helperText={touched.noOfEmployees && errors.noOfEmployees}
+                    />
+                </section>
+                <section className='actions'>
+                    {isSubmitting && <CircularProgress />}
+                    {!isSubmitting && <React.Fragment>
+                        <Button className='cancelBtn' onClick={cancel}>Cancel</Button>
+                        <Button className={isValid ? 'submitBtn' : ''} disabled={!isValid} onClick={handleSubmit}>Create company</Button>
+                    </React.Fragment>}
+                </section>
             </Grid>
-        </div>
-    )
-}
+        </Grid>
+    </div>
+);
 
-const NewCompanyHOC = compose(
-    withRouter,
-    graphql(setFeedbackMessage, { name: 'setFeedbackMessage' }),
-    withFormik({
-        mapPropsToValues: () => ({
-            name: '',
-            industry: '',
-            noOfEmployees: '',
-            location: ''
-        }),
-        validationSchema: schema,
-        handleSubmit: async (values, { props: { handleCompany, match, history, setFeedbackMessage }, setSubmitting }) => {
-            try {
-                await handleCompany({
-                    variables: {
-                        language: match.params.lang,
-                        details: values
-                    },
-                    refetchQueries: [
-                        companiesRefetch(match.params.lang)
-                    ]
-                });
-
-                await setFeedbackMessage({
-                    variables: {
-                        status: 'success',
-                        message: 'Changes saved successfully.'
-                    }
-                });
-
-                history.push(`/${match.params.lang}/companies`);
-
-            } catch (err) {
-                await setFeedbackMessage({
-                    variables: {
-                        status: 'error',
-                        message: err.message
-                    }
-                });
-            }
-            setSubmitting(false);
-        },
-        displayName: 'AddCompanyForm', // helps with React DevTools
-    }),
-    pure
-)
-
-export default NewCompanyHOC(NewCompany);
+export default NewCompany;
--- src/pages/companies/new/container.js
+++ src/pages/companies/new/container.js
@@ -1,100 +1,55 @@
 import NewCompany from './component';
-import { compose, withState, withHandlers, pure } from 'recompose';
-// import uuid from 'uuid/v4';
+import { compose, pure } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import { graphql } from 'react-apollo';
-import { handleCompany } from '../../../store/queries';
+import { withFormik } from 'formik';
+
+import schema from './validation';
+import { handleCompany, setFeedbackMessage } from '../../../store/queries';
+import { companiesRefetch, currentProfileRefetch } from '../../../store/refetch';
 
 const NewCompanyHOC = compose(
     withRouter,
     graphql(handleCompany, { name: 'handleCompany' }),
-    // withState('formData', 'setFormData', { id: uuid() }),
-    withState('uploadProgress', 'setUploadProgress', 0),
-    withState('uploadError', 'setUploadError', null),
-    withState('isUploading', 'setIsUploading', false),
-    withHandlers({
-        getSignedUrl: ({ location: { state: { profile } } }) => async (file, callback) => {
-            let getExtension = file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
-            let fName = ['logo', getExtension].join('.');
-
-            const params = {
-                fileName: fName,
-                contentType: file.type,
-                id: profile.id,
-                type: 'logo'
-            };
-
+    graphql(setFeedbackMessage, { name: 'setFeedbackMessage' }),
+    withFormik({
+        mapPropsToValues: () => ({
+            name: '',
+            industry: '',
+            noOfEmployees: '',
+            location: ''
+        }),
+        validationSchema: schema,
+        handleSubmit: async (values, { props: { handleCompany, match, history, setFeedbackMessage }, setSubmitting }) => {
             try {
-                let response = await fetch('https://k73nyttsel.execute-api.eu-west-1.amazonaws.com/production/getSignedURL', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
+                await handleCompany({
+                    variables: {
+                        language: match.params.lang,
+                        details: values
                     },
-                    body: JSON.stringify(params)
+                    refetchQueries: [companiesRefetch(match.params.lang), currentProfileRefetch(match.params.lang)]
                 });
-                let responseJson = await response.json();
-                callback(responseJson);
-            } catch (error) {
-                console.error(error);
-                callback(error)
-            }
-        },
-        onUploadStart: ({ setIsUploading }) => (file, next) => {
-            let size = file.size;
-            if (size > 500 * 1024) {
-                alert('File is too big!');
-            } else {
-                setIsUploading(true);
-                next(file);
-            }
-        },
-        onProgress: ({ setUploadProgress }) => (percent) => {
-            setUploadProgress(percent);
-        },
-        onError: ({ setUploadError }) => error => {
-            setUploadError(error);
-            console.log(error);
-        },
-        onFinishUpload: (props) => async () => {
-            const { setIsUploading/*, updateAvatar, updateAvatarTimestamp, match*/ } = props;
-            // await updateAvatar({
-            //     variables: {
-            //         status: true
-            //     },
-            //     refetchQueries: [{
-            //         query: currentUserQuery,
-            //         fetchPolicy: 'network-only',
-            //         name: 'currentUser',
-            //         variables: {
-            //             language: 'en',
-            //             id: match.params.profileId
-            //         }
-            //     }]
-            // });
-            // await updateAvatarTimestamp({
-            //     variables: {
-            //         timestamp: Date.now()
-            //     }
-            // });
 
-            setIsUploading(false);
+                await setFeedbackMessage({
+                    variables: {
+                        status: 'success',
+                        message: 'Changes saved successfully.'
+                    }
+                });
+
+                history.push(`/${match.params.lang}/companies`);
+
+            } catch (err) {
+                await setFeedbackMessage({
+                    variables: {
+                        status: 'error',
+                        message: err.message
+                    }
+                });
+            }
+            setSubmitting(false);
         },
-        // handleFormChange: ({ setFormData }) => event => {
-        //     const target = event.currentTarget;
-        //     const value = target.type === 'checkbox' ? target.checked : target.value;
-        //     const name = target.name;
-        //     if (!name) {
-        //         throw Error('Field must have a name attribute!');
-        //     }
-        //     setFormData(state => ({ ...state, [name]: value }));
-        // },
-        // cancel: ({ history }) => () => {
-        //     history.goBack()
-        // },
-        // save: ({ formData }) => () => {
-        //     console.log(formData);
-        // }
+        displayName: 'AddCompanyForm', // helps with React DevTools
     }),
     pure
 );
