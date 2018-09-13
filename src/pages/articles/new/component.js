307c7f9a0e79422b741be203777c4b66d1c0e63b
robbieve <robbieeve751994@gmail.com>
2018-09-13 10:34:04 +0300

    Add images to Froala. Select featured image. Display featured image on newsfeed.

--- src/pages/articles/new/component.js
+++ src/pages/articles/new/component.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, TextField, Button, FormGroup, FormLabel, Switch as ToggleSwitch } from '@material-ui/core';
-// import S3Uploader from 'react-s3-uploader';
+import { Grid, TextField, Button, Popover, IconButton, Icon, Checkbox } from '@material-ui/core';
+import ReactPlayer from 'react-player';
 
 // Require Editor JS files.
 import 'froala-editor/js/froala_editor.pkgd.min.js';
@@ -12,125 +12,193 @@ import 'font-awesome/css/font-awesome.css';
 import FroalaEditor from 'react-froala-wysiwyg';
 import ImageUploader from '../../../components/imageUploader';
 import TagsInput from '../../../components/TagsInput';
+import { s3BucketURL } from '../../../constants/s3';
 
-const NewArticle = ({
-    handleFormChange, state: { formData: { id, title, description, videoURL, tags }, isVideoUrl, imageUploadOpen },
-    updateDescription, saveArticle, switchMediaType,
-    openImageUpload, closeImageUpload, handleError, handleSuccess,
-    getSignedURL, handleFroalaSuccess, handleFroalaError,
-    setTags
-}) => (
-    <div className='newArticleRoot'>
-        <Grid container className='mainBody articleEdit'>
-            <Grid item lg={6} md={6} sm={10} xs={11} className='centralColumn'>
-                <section className='titleSection'>
-                    <TextField
-                        // error={touched.name && errors.name}
-                        // helperText={errors.name}
-                        name="title"
-                        label="Title"
-                        placeholder="Add title..."
-                        className='textField'
-                        fullWidth
-                        onChange={handleFormChange}
-                        value={title || ''}
-                        InputProps={{
-                            classes: {
-                                input: 'titleInput',
-                            }
-                        }}
-                    />
-                </section>
-                <section className='articleBodySection'>
-                    <p className='infoMsg'>Write your article below.</p>
-                    <FroalaEditor
-                        config={{
-                            placeholderText: 'Article body...',
-                            iconsTemplate: 'font_awesome_5',
-                            toolbarInline: true,
-                            charCounterCount: false,
-                            imageUploadRemoteUrls: false,
-                            toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'fontFamily', 'fontSize', 'color', '-', 'paragraphFormat', 'align', 'formatOL', 'indent', 'outdent', '-', 'undo', 'redo'],
-                            events: {
-                                'froalaEditor.image.beforeUpload': getSignedURL,
-                                'froalaEditor.image.uploaded': handleFroalaSuccess,
-                                'froalaEditor.image.error': handleFroalaError
-                            }
-                        }}
-                        model={description}
-                        onModelChange={updateDescription}
-                    />
-                </section>
-                <section className='mediaUpload'>
+const NewArticle = (props) => {
+    const {
+        handleFormChange, state: { formData: { id, title, description, tags }, imageUploadOpen, videoShareAnchor, videoURL, isVideoUrlValid },
+        updateDescription, saveArticle,
+        openImageUpload, closeImageUpload, handleError, handleSuccess,
+        setTags, bindEditor, removeImage,
+        openVideoShare, closeVideoShare, updateVideoUrl,
+        images, selectFeaturedImage, videos, selectFeaturedVideo
+    } = props;
 
-                </section>
-            </Grid>
-            <Grid item lg={3} md={3} sm={10} xs={11} className='columnRight'>
-                <div className='columnRightContent'>
-                    <section className='tags'>
-                        <p className='helperText'>Tag your article</p>
-                        <TagsInput value={tags} onChange={setTags} helpTagName='tag' />
-                    </section>
-
-                    <FormGroup row className='mediaToggle'>
-                        <span className='mediaToggleLabel'>Upload visuals</span>
-                        <FormLabel className={!isVideoUrl ? 'active' : ''}>Photo</FormLabel>
-                        <ToggleSwitch
-                            checked={isVideoUrl}
-                            onChange={switchMediaType}
-                            classes={{
-                                switchBase: 'colorSwitchBase',
-                                checked: 'colorChecked',
-                                bar: 'colorBar',
+    // debugger;
+    return (
+        <div className='newArticleRoot'>
+            <Grid container className='mainBody articleEdit'>
+                <Grid item lg={6} md={6} sm={10} xs={11} className='centralColumn'>
+                    <section className='titleSection'>
+                        <TextField
+                            name="title"
+                            label="Title"
+                            placeholder="Add title..."
+                            className='textField'
+                            fullWidth
+                            onChange={handleFormChange}
+                            value={title}
+                            InputProps={{
+                                classes: {
+                                    input: 'titleInput',
+                                }
                             }}
-                            color="primary" />
-                        <FormLabel className={isVideoUrl ? 'active' : ''}>Video Url</FormLabel>
-                    </FormGroup>
-
-                    <section className='mediaUpload'>
-                        {isVideoUrl ?
-                            <TextField
-                                name="videoURL"
-                                label="Add video URL"
-                                placeholder="Video URL..."
-                                className='textField'
-                                onChange={handleFormChange}
-                                value={videoURL || ''}
-                                fullWidth
-                                InputProps={{
-                                    classes: {
-                                        input: 'textFieldInput',
-                                        underline: 'textFieldUnderline'
-                                    },
-                                }}
-                                InputLabelProps={{
-                                    className: 'textFieldLabel'
-                                }}
-                            /> :
-
-                            <React.Fragment>
-                                <Button className='imgUpload' onClick={openImageUpload}>
-                                    Upload
-                            </Button>
-                                <ImageUploader
-                                    type
-                                    id={id}
-                                    open={imageUploadOpen}
-                                    onClose={closeImageUpload}
-                                    onError={handleError}
-                                    onSuccess={handleSuccess}
-                                />
-                            </React.Fragment>
-                        }
+                        />
                     </section>
+                    <section className='mediaUpload'>
+                        <p className='helperText'>
+                            Add/Edit images or embed video links.
+                        </p>
 
-                    <Button className='publishBtn' onClick={saveArticle}>
-                        Publish article
-                    </Button>
-                </div>
+                        <Button className='mediaBtn' onClick={openImageUpload}>
+                            Add image
+                        </Button>
+
+                        <ImageUploader
+                            type='article'
+                            open={imageUploadOpen}
+                            onClose={closeImageUpload}
+                            onError={handleError}
+                            onSuccess={handleSuccess}
+                            id={id}
+                        />
+                        {/*
+                        <Button className='mediaBtn' onClick={openVideoShare}>
+                            Share video
+                        </Button>
+
+                        <Popover
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            open={Boolean(videoShareAnchor)}
+                            anchorEl={videoShareAnchor}
+                            onClose={closeVideoShare}
+                            classes={{
+                                paper: 'promoEditPaper'
+                            }}
+                            disableBackdropClick
+                        >
+                            <div className='popupBody'>
+                                <TextField
+                                    name="videoUrl"
+                                    label="Video URL"
+                                    placeholder="Enter video link..."
+                                    className='textField'
+                                    fullWidth
+                                    onChange={updateVideoUrl}
+                                    value={videoURL}
+                                    helperText={(!!videoURL && !isVideoUrlValid && 'Invalid video URL')}
+                                    error={!!videoURL && !isVideoUrlValid}
+                                />
+                            </div>
+                            <div className='popupFooter'>
+                                <IconButton
+                                    onClick={closeVideoShare}
+                                    className='footerCheck'
+                                    disabled={!!videoURL && !isVideoUrlValid}
+                                >
+                                    <Icon>done</Icon>
+                                </IconButton>
+                            </div>
+                        </Popover>
+*/}
+
+                    </section>
+                    <section className='articleBodySection'>
+                        <p className='infoMsg'>Write your article below.</p>
+                        <FroalaEditor
+                            config={{
+                                placeholderText: 'Article body...',
+                                iconsTemplate: 'font_awesome_5',
+                                toolbarInline: true,
+                                charCounterCount: false,
+                                imageUploadRemoteUrls: false,
+                                toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'fontFamily', 'fontSize', 'color', '-', 'paragraphFormat', 'align', 'formatOL', 'indent', 'outdent', '-', 'undo', 'redo'],
+                                events: {
+                                    'froalaEditor.initialized': bindEditor,
+                                    'froalaEditor.image.removed': removeImage
+                                }
+                            }}
+                            model={description}
+                            onModelChange={updateDescription}
+                        />
+                    </section>
+                </Grid>
+                <Grid item lg={3} md={3} sm={10} xs={11} className='columnRight'>
+                    <div className='columnRightContent'>
+                        <section className='tags'>
+                            <p className='helperText'>Tag your article</p>
+                            <TagsInput value={tags} onChange={setTags} helpTagName='tag' />
+                        </section>
+
+                        {((images && images.length > 0) || (videos && videos.length > 0)) &&
+                            <section className='featuredMedia'>
+                                <p className='helperText'>
+                                    Select a cover image for your article.
+                                </p>
+                                {(images && images.length > 0) &&
+                                    <div className='images'>
+                                        {images.map(image => (
+                                            <label key={image.id} className={image.isFeatured ? 'selected' : null}>
+                                                <Checkbox
+                                                    checked={image.isFeatured}
+                                                    onChange={() => selectFeaturedImage(image.id)}
+                                                    className='hiddenInput'
+                                                />
+                                                <img src={`${s3BucketURL}${image.path}`} className='featuredImg' alt={image.id} />
+                                                {image.isFeatured && <i className='fas fa-check fa-3x' />}
+                                            </label>
+                                        ))}
+                                    </div>
+                                }
+                                {(videos && videos.length > 0) &&
+                                    <div className='videos'>
+                                        {videos.map(video => (
+                                            <label key={video.id} className={video.isFeatured ? 'selected' : null}>
+                                                <Checkbox
+                                                    checked={video.isFeatured}
+                                                    onChange={() => selectFeaturedVideo(video.id)}
+                                                    className='hiddenInput'
+                                                />
+                                                <ReactPlayer
+                                                    url={video.path}
+                                                    width='100%'
+                                                    height='100%'
+                                                    config={{
+                                                        youtube: {
+                                                            playerVars: {
+                                                                showinfo: 0,
+                                                                controls: 0,
+                                                                modestbranding: 1,
+                                                                loop: 1
+                                                            }
+                                                        }
+                                                    }}
+                                                    playing={false}
+                                                    style={{ pointerEvents: 'none' }}
+                                                />
+                                                {video.isFeatured && <i className='fas fa-check fa-3x' />}
+                                            </label>
+                                        ))}
+                                    </div>
+                                }
+                            </section>
+                        }
+
+                        <Button className='publishBtn' onClick={saveArticle}>
+                            Publish article
+                        </Button>
+                    </div>
+                </Grid>
             </Grid>
-        </Grid>
-    </div>
-);
+        </div>
+    );
+};
 
 export default NewArticle;
